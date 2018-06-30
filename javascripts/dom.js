aebd1452c0cec64e9a4a190a3f7f78c28d68c431
Maken Hubbard <makhub26@outlook.com>
2018-06-29 23:27:19 -0500

    found some issues and have no clue why they happened restart cpomputer wip

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -21,33 +21,12 @@ const buildDomForProjects = () => {
       let projectString = '';
       projectsArray.forEach((project) => {
         projectString += `<div class="jumbotron">`;
-        projectString += `<img src="../images/localWeather.jpg" rel="sign-up-page">`;
         projectString += `<h1>${project.title}</h1>`;
         projectString += `<p>${project.description}</p>`;
         projectString += `<span> Technologies used: ${project.technologiesUsed}</span>`;
         projectString += `<ul>`;
-        projectString += `<li>URL: ${project.url}</li>`;
-        projectString += `<li>Github: ${project.github}`;
-        projectString += `</ul>`;
-        projectString += `</div >`;
-      });
-      printProjectsToDom(projectString);
-    });
-};
-
-const learnMoreDom = () => {
-  firebaseApi.getProjectsFromFirebase()
-    .then((projectsArray) => {
-      let projectString = '';
-      projectsArray.forEach((project) => {
-        projectString += `<div class="jumbotron">`;
-        projectString += `<img src="../images/localWeather.jpg" rel="sign-up-page">`;
-        projectString += `<h1>${project.title}</h1>`;
-        projectString += `<p>${project.description}</p>`;
-        projectString += `<span> Technologies used: ${project.technologiesUsed}</span>`;
-        projectString += `<ul>`;
-        projectString += `<li>URL: ${project.url}</li>`;
-        projectString += `<li>Github: ${project.github}`;
+        projectString += `<li>URL: <a href="${project.url}">Click Here</a></li>`;
+        projectString += `<li>Github: <a href="${project.github}">Click Here</a>`;
         projectString += `</ul>`;
         projectString += `</div >`;
       });
@@ -66,5 +45,4 @@ const printProjectsToDom = (project) => {
 module.exports = {
   buildDomForBlogs,
   buildDomForProjects,
-  learnMoreDom,
 };
